d8c2acaa174e66efc563664fa77524d01f4876d4
KcinKcip <fedor_pushko@mail.ru>
2019-02-04 00:12:37 +0300

    First commit
    My first commit

---
+++ Assets/Scripts/CreateSquare.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSquare : MonoBehaviour
+{
+    public Sprite destroyedSquare;
+    public Sprite shipSquare;
+    public Sprite missSquare;
+    void OnMouseDown()
+    {
+        if (!gameObject.GetComponent<SpriteRenderer>().sprite.Equals(missSquare) && !gameObject.GetComponent<SpriteRenderer>().sprite.Equals(destroyedSquare))
+        {
+            if (gameObject.GetComponent<SpriteRenderer>().sprite.Equals(shipSquare))
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = destroyedSquare;
+                CreateField.playerHits++;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = missSquare;
+                CreateField.playerTurn = false;
+            }
+            gameObject.transform.Translate(0, 0, -3);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TextScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextScript : MonoBehaviour
+{
+    public Text textScore;
+    public Text textWin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textScore.text = "Score: Player 0 - 0 AI";
+        textWin.text = "";
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        textScore.text = "Score: Player " + CreateField.playerHits + " - " + CreateField.aiHits + " AI";
+        if (CreateField.playerHits == 20) { textWin.text = "Player Win!"; }
+        if (CreateField.aiHits == 20) { textWin.text = "AI Win!"; }
+    }
+}
